e6f52ae7bbab16ad8e255d8a774df67ae036849d
Dave Rush <northwoodlogic@gmail.com>
2018-12-22 14:17:52 -0600

    build bare-bones ERL3 kernel+mfs appliance image
    Build self contained Edge Router Lite kernel + embedded MFS appliance.
    The kernel can be can be loaded via TFTP or from msdosfs filesystem on
    USB stick.
    
        cd appliance/erl_mini
        ./build-erl-mini.sh
    
    The kernel is roughly 36MB in size and contains all the bits needed
    for a rescue or network deployment system. Currently it doesn't do
    anything other than boot up to a shell login prompt. The kernel can be
    found at 'mips64-mini.kernel/boot/kernel/kernel' relative to the
    erl_mini appliance build directory.

---
+++ appliance/erl_mini/build-erl-mini.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+
+set -x
+set -e
+
+DESTDIR=$(pwd)/mips64-mini
+SRCCONF=$(pwd)/src.conf
+MINI_IMG=$(pwd)/mips64-mini.img
+export TARGET=mips
+export TARGET_ARCH=mips64
+export KERNCONF=ERL_MINI
+export DESTDIR
+
+PM=$(sysctl -n hw.ncpu)
+
+# Need to be in top level source directory.
+cd ../../
+
+sed -e "s^@mfs_image@^${MINI_IMG}.uzip^g" \
+	< sys/mips/conf/ERL_MINI.in \
+	> sys/mips/conf/ERL_MINI
+
+if [ -e ${DESTDIR} ] ; then
+	chflags -R noschg ${DESTDIR}
+	rm -Rf ${DESTDIR}
+fi
+
+if [ -e ${MINI_IMG} ] ; then
+	rm ${MINI_IMG}
+fi
+
+make SRCCONF=${SRCCONF} -j${PM} buildworld
+echo "DONE BUILDING WORLD"
+
+make SRCCONF=${SRCCONF} installworld
+echo "DONE INSTALLING WORLD"
+
+make SRCCONF=${SRCCONF} distribution
+echo "DONE DISTRIBUTE WORLD"
+
+(
+	cd ${DESTDIR}
+	# don't want static archives
+	rm usr/lib/*.a
+	# uzip rootfs is ro, no need to fsck
+	echo "/dev/md0.uzip / ufs ro 0 0" > etc/fstab
+
+	cat << EOT > etc/rc.conf
+root_rw_mount="NO"
+hostid_enable="NO"
+hostname="edge"
+EOT
+
+)
+makefs -B big -t ffs ${MINI_IMG} ${DESTDIR}
+mkuzip -o ${MINI_IMG}.uzip ${MINI_IMG}
+
+unset NO_CLEAN
+make SRCCONF=${SRCCONF} -j${PM} buildkernel
+DESTDIR=${DESTDIR}.kernel make SRCCONF=${SRCCONF} installkernel
+
+#
+# Self contained 'appliance' kernel+rootfs is located here:
+#   ${DESTDIR}.kernel/boot/kernel/kernel
+#
+# Kernel is approximately 36MB
+#
+# Octeon ubnt_e100# dhcp
+# Interface 0 has 3 ports (RGMII)
+# BOOTP broadcast 1
+# octeth0: Up 1000 Mbps Full duplex (port  0)
+# DHCP client bound to address 10.0.0.33
+#
+# Octeon ubnt_e100# setenv serverip 10.0.0.31
+#
+# Octeon ubnt_e100# tftp $loadaddr kernel
+# Using octeth0 device
+# TFTP from server 10.0.0.31; our IP address is 10.0.0.33
+# Filename 'kernel'.
+# Load address: 0x9f00000
+# Loading: #################################################################
+#          #################################################################
+#          #################################################################
+#          #################################################################
+#          ######
+# done
+# Bytes transferred = 37974056 (2437028 hex), 5579 Kbytes/sec
+#
+# Octeon ubnt_e100# bootoctlinux $loadaddr coremask=0x3
+

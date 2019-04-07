305e094620addb024329658b9e956b445eaff32e
Dave Rush <northwoodlogic@gmail.com>
2019-04-07 09:05:53 -0500

    Increase EFI staging area size, add vmm kernel module to amd64 appliance
    The AMD64 uzip image is approaching the 64MB max size of the
    default EFI staging area. Increase the size using a make variable.
    Build vmm as a module and clean up the appliance building script
    a bit.

--- appliance/amd64_mini/build-amd64-mini.sh
+++ appliance/amd64_mini/build-amd64-mini.sh
@@ -11,8 +11,17 @@ MINI_IMG=$(pwd)/amd64-mini.img
 export KERNCONF=AMD64_MINI
 export DESTDIR
 
-# 64MB that's reserved in loader.efi
+# 64MB
 MD_IMAGE_SIZE=67108864
+
+# 80MB
+#MD_IMAGE_SIZE=83886080
+
+# The default EFI staging area is 64MB. That's not big enough for us so
+# double it to 128MB (even though the config above makes a 64MB laoder).
+# Note: The total staging area must be in the first
+# 1GB of memory.
+EFI_STAGING_SIZE=128
 
 PM=$(sysctl -n hw.ncpu)
 
@@ -37,7 +46,7 @@ if [ -e ${BOOTDIR} ] ; then
 	rm -Rf ${BOOTDIR}
 fi
 
-make SRCCONF=${SRCCONF} MD_IMAGE_SIZE=${MD_IMAGE_SIZE} -j${PM} buildworld
+make SRCCONF=${SRCCONF} EFI_STAGING_SIZE=${EFI_STAGING_SIZE} MD_IMAGE_SIZE=${MD_IMAGE_SIZE} -j${PM} buildworld
 echo "DONE BUILDING WORLD"
 
 make SRCCONF=${SRCCONF} installworld
@@ -58,6 +67,10 @@ echo "DONE DISTRIBUTE WORLD"
 		rm -Rf usr/lib32/*.a
 	fi
 
+	if [ -d "usr/include" ] ; then
+		rm -Rf usr/include
+	fi
+
 	# uzip rootfs is ro, no need to fsck
 	echo "/dev/md0.uzip / ufs ro 0 0" > etc/fstab
 
@@ -71,9 +84,18 @@ echo "DONE DISTRIBUTE WORLD"
 	hostname="xbot"
 	root_rw_mount="NO"
 	entropy_file="NO"
-	hostid_enable="NO"
 	sshd_enable="YES"
-	#ifconfig_ue0_ipv6="auto_linklocal accept_rtadv"
+	zfs_enable="YES"
+	powerd_enable="YES"
+	rtsold_enable="YES"
+	ifconfig_mlxen0_ipv6="auto_linklocal accept_rtadv"
+
+	kld_list="vmm"
+	EOT
+
+	cat <<-EOT > etc/sysctl.conf
+	vfs.zfs.min_auto_ashift=12
+	net.link.tap.up_on_open=1
 	EOT
 
 	# Trigger rc.initcfg
@@ -84,19 +106,21 @@ echo "DONE DISTRIBUTE WORLD"
 	touch etc/diskless
 	mkdir -p conf/base/etc
 	tar -cf - -C etc/ . | tar -xf - -C conf/base/etc/
+
+	# scratch dir for mounting disks, if applicable
+	mkdir data
 )
 
+# Each one of the "loaders" will allocate 64MB of space which ends
+# up being > 500MB of stuff used up under /boot. All we need is the
+# loader_simp.efi, a couple config files, kernel, & md_root
 mv ${DESTDIR}/boot ${BOOTDIR}
-makefs -B little -t ffs ${MINI_IMG} ${DESTDIR}
-mkuzip -o ${MINI_IMG}.uzip ${MINI_IMG}
 
-# TODO: change this so the md image isn't linked into the kernel. we
-# need loader anyway so let it load the image as a module.
-unset NO_CLEAN
 make SRCCONF=${SRCCONF} -j${PM} buildkernel
-DESTDIR=${DESTDIR}.kernel make SRCCONF=${SRCCONF} installkernel
+DESTDIR=${DESTDIR} make SRCCONF=${SRCCONF} installkernel
 
 cd ${HERE}
+
 cp ${BOOTDIR}/loader_simp.efi amd64-mini.efi
 rm   -Rf md-boot
 mkdir -p md-boot/boot/defaults
@@ -104,8 +128,31 @@ mkdir -p md-boot/boot/kernel
 
 cp ${BOOTDIR}/device.hints              md-boot/boot/
 cp ${BOOTDIR}/defaults/loader.conf      md-boot/boot/defaults/
-cp ${DESTDIR}.kernel/boot/kernel/kernel md-boot/boot/kernel/
-echo 'hw.mfi.mrsas_enable="1"' >>       md-boot/boot/device.hints
+
+# This needs to happen after the kernel is moved out of DESTDIR
+# but before copying the u-zip image into md-root/boot
+mv ${DESTDIR}/boot/kernel/kernel	md-boot/boot/kernel/
+makefs -B little -t ffs ${MINI_IMG} ${DESTDIR}
+mkuzip -o ${MINI_IMG}.uzip ${MINI_IMG}
+
+cp ${MINI_IMG}.uzip			md-boot/boot/
+
+# loader_simp.efi doesn't have a script interpreter. Use commands
+# directly in loader.rc. Loader does a sanity check which involves
+# checking for two files:
+#
+#	/boot/kernel/kernel
+#	/boot/defaults/loader.conf
+#
+cat << EOT > md-boot/boot/loader.rc
+unload
+load             /boot/kernel/kernel
+load -t md_image /boot/amd64-mini.img.uzip
+
+set hw.mfi.mrsas_enable="1"
+set kern.geom.label.disk_ident.enable="0"
+set kern.geom.label.gptid.enable="0"
+EOT
 
 makefs md-boot.img md-boot
 sh ../../sys/tools/embed_mfs.sh amd64-mini.efi md-boot.img
